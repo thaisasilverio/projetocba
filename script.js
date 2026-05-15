7018692b50782981689b35c41b69ff3f8c28ef17
Thaisa Vitoria Fernandes Silverio <thaisa.silverio@cba.com.br>
2026-05-15 10:40:03 -0300

    db

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 function feedback() {
     let data = document.getElementById("data").value;
     let forno = document.getElementById("forno").value;
-    let EA = parseFloat(document.getElementById("EA").value);
-    let tensao = parseFloat(document.getElementById("tensao").value);
+    let ea = parseFloat(document.getElementById("ea").value);
+    //let tensao = parseFloat(document.getElementById("tensao").value);
     let temperatura = parseFloat(document.getElementById("temperatura").value);
+    let db = parseFloat(document.getElementById("db").value);
     let descricao = document.getElementById("descricao").value;
     let resultado = "";
     resultado += "RELATÓRIO DO FORNO\n\n";
@@ -13,21 +14,22 @@ function feedback() {
     resultado += "Forno: " + forno + "\n\n";
 
     //EA
-    if (EA <= 0.22) {
-        resultado += "\nEA:\n" + "Forno não está saudável\n";
-    } else if (EA >= 0.31) {
-        resultado +=  "\nEA:\n" + "Forno está saudável\n";
+    if (ea <= 0.22) {
+        resultado += "\n FREQUÊNCIA EA:\n" + "A frequência do EA está fora do padrão\n";
+    } else if (ea >= 0.31) {
+        resultado +=  "\nEA:\n" + "A frequência do EA está de acordo com o padrão\n";
     } else {
         resultado +=  "\nEA:\n" + "Necessário avaliação\n";
     }
     //tensão
-    if (tensao <= 4.32) {
-        resultado += "\nTensão:\n" + "Possível queda de temperatura e sujeito a banho sujo\n";
-    } else if (tensao >= 4.37) {
-        resultado += "\nTensão:\n" + "Possível aumento na temperatura e desgaste no ânodo\n";
-    } else {
-        resultado += "\nTensão:\n" + "A tensão está ideal\n";
-    }
+    //if (tensao <= 4.32) {
+    //    resultado += "\nTensão:\n" + "Possível queda de temperatura e sujeito a banho sujo\n";
+   // } else if (tensao >= 4.37) {
+    //    resultado += "\nTensão:\n" + "Possível aumento na temperatura e desgaste no ânodo\n";
+   // } else {
+   //     resultado += "\nTensão:\n" + "A tensão está ideal\n";
+   // }
+   
     //temperatura
     if (temperatura <= 954) {
         resultado += "\nTemperatura:\n" + "A temperatura está baixa, forno sujeito a banho sujo\n";
@@ -35,6 +37,12 @@ function feedback() {
         resultado += "\nTemperatura:\n" + "Possível aumento na temperatura e desgaste no ânodo\n";
     } else {
         resultado += "\nTemperatura:\n" + "Temperatura está no padrão\n";
+    }
+   //db
+    if (db <= 0.15){
+        resultado += "\nDB:\n" + "O DB está de acordo com o padrão\n";
+    }else{
+        resultado += "\nDB:\n" + "Excesso de alumina, sujeito a lama no banho\n";
     }
 
     resultado += "\nDescrição:\n" + descricao;
